07a1c7602ef84fd2550d0570809f9c051d1ff7e7
Guilherme Vianna <mataveli91@gmail.com>
2022-12-05 22:33:09 -0300

    Initial Commit

--- src/Main/java/View/Connect_GUI.java
+++ src/Main/java/View/Connect_GUI.java
@@ -5,9 +5,8 @@ import javax.swing.*;
 public class Connect_GUI extends JFrame{
     private JButton button1;
     private JPanel mainPanel;
-    public Connect_GUI(String Name){
+    public Connect_GUI(String Name){ //TODO IMPLEMENT
         super(Name);
-
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
