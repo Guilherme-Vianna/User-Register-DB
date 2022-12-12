c63679ff43a36c77d2fa0108502a10aa6f59766b
Guilherme Vianna <mataveli91@gmail.com>
2022-12-11 22:18:02 -0300

    Base View Created
    Base View Created

--- src/Main/java/Controller/Main.java
+++ src/Main/java/Controller/Main.java
@@ -1,8 +1,6 @@
 package Controller;
-import View.Connect_GUI;
 
 import javax.swing.*;
-import java.net.ConnectException;
 
 public class Main {
     public static void main(String args[]){
--- src/Main/java/View/Connect_GUI.java
+++ src/Main/java/View/Connect_GUI.java
@@ -1,14 +1,4 @@
 package View;
 
-import javax.swing.*;
-
-public class Connect_GUI extends JFrame{
-    private JButton button1;
-    private JPanel mainPanel;
-    public Connect_GUI(String Name){ //TODO IMPLEMENT
-        super(Name);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setContentPane(mainPanel);
-        this.pack();
-    }
+public class Connect_GUI {
 }
